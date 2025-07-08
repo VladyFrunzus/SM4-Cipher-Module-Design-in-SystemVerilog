16922ab78e5462359cf25c4f49945eda83e81ce4
Vladimir Frunza <127752888+VladyFrunzus@users.noreply.github.com>
2025-07-08 23:03:15 +0300

    Delete SM4_Finale.srcs directory

--- SM4_Finale.srcs/sources_1/new/rotate_left_32bit.sv
+++
@@ -1,9 +0,0 @@
-function automatic logic [31:0] rotate_left_32bit (input logic [31:0] v, 
-                                                   input int unsigned s);
-				
-        int unsigned res;
-        int unsigned s_mod = s & 31; // modulo 31, a.i. sa nu avem 0 la iesire
-        rotate_left_32bit = (v << s_mod) | (v >> (32 - s_mod));
-        res = rotate_left_32bit;
-
-endfunction
